23416d71094ae9daa8beff477b13ed0a0473bcbb
thiago-you <thiago.youx@gmail.com>
2023-01-26 13:06:24 -0300

    update packages

--- carouselview/src/main/java/com/synnapps/carouselview/ImageClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.synnapps.carouselview;
-
-/**
- * Created by leonardo on 06/11/16.
- */
-
-public interface ImageClickListener {
-    void onClick(int position);
-}
---
+++ carouselview/src/main/java/you/thiago/carouselview/ImageClickListener.java
@@ -0,0 +1,9 @@
+package you.thiago.carouselview;
+
+/**
+ * Created by leonardo on 06/11/16.
+ */
+
+public interface ImageClickListener {
+    void onClick(int position);
+}
